23565ab948f4397ff843680d2cc789543f4b222b
tes <nabilaalrasyid@gmail.com>
2023-06-26 20:57:40 +0700

    Tugas

---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <!--banner image-->
+
+  <div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
+    <div class="content text-center">
+      <h1 class="text-white">WELCOME TO E-BOOK! </h1>
+        <h2> Enjoy Your Reading</h2>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+.banner-image{
+  background: #6b6b6b;
+  font-family: "Lucida Bright" ;
+  font-size: 30px;
+
+}
+
+h2 {
+    font-style: italic;
+    font-size: 20px;
+    padding-top: 50px;
+
+}
+
+</style>
